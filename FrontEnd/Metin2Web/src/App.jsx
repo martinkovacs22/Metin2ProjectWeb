470cdb13762622f88a55281d310c3164257a97d9
martinkovacs22 <martinkovacs22@gmail.com>
2024-06-11 04:59:37 +0200

    transfrormationTheLanguageJSON

--- FrontEnd/Metin2Web/src/App.jsx
+++ FrontEnd/Metin2Web/src/App.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Transformation from "./config/transformation.jsx";
 import Home from "./home/home.jsx";
-import "./index.css"
-import "./base.css"
+import "./index.css";
+import "./base.css";
 
 function App() {
+  const [components, setComponents] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [components,setComponents] = useState([]);
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false); // A betöltés befejeződött
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
 
   const addComponentToTransformation = ({ adderComponents }) => {
     if (Array.isArray(adderComponents)) {
@@ -16,14 +28,16 @@ function App() {
         }
       });
     }
-
-  }
+  };
 
   return (
-    <>
-      <Home addComponentToTransformation={addComponentToTransformation} />
-      <Transformation to={"hun"} components={components} />
-    </>
+    <div >
+      
+          <Home addComponentToTransformation={addComponentToTransformation} />
+          <Transformation to={"hun"} components={components} />
+     
+ 
+    </div>
   );
 }
 
